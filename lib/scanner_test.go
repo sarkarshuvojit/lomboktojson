94aa7a9e07b5c966176e071b00b840b6a61f3007
Shuvojit <s15sarkar@yahoo.com>
2024-07-01 19:40:08 +0530

    nested object parsing added

--- lib/scanner_test.go
+++ lib/scanner_test.go
@@ -76,8 +76,6 @@ func Test_ScanTheCustomer(t *testing.T) {
 		scanner := lib.NewScanner(&sourceBuf)
 		tokens := scanner.Scan()
 
-		// Customer | ParenOpen | Key | EQUALS | Value | PAREN_CLOSE | EOF
-		// 0			1			2	3		4		5			6
 		expectedTokenLen := 7
 		if len(tokens) != expectedTokenLen {
 			t.Errorf("Should return %d token for EOF, got %d", expectedTokenLen, len(tokens))
@@ -150,23 +148,28 @@ func Test_ScanTheCustomerWithMultipleFieldsFlat(t *testing.T) {
 }
 
 func Test_ScanTheCustomerWithMultipleFieldsNested(t *testing.T) {
-	source := `Customer(name=Rajesh Kumar,age=50,email=rajesh@kumar.com)`
-	t.Run("Customer(name, age, email): TokenLengh & Lexemes", func(t *testing.T) {
+	source := `Customer(name=Michael Scott, age=40, email=michael@dundermifflin.com, address=Address(billingAddress=Some random address, shippingAddress=Some random address again))`
+	t.Run("Customer(name, age, email, Address(billingAddress,shippingAddress)): TokenLengh & Lexemes", func(t *testing.T) {
 		var sourceBuf bytes.Buffer
 		sourceBuf.WriteString(source)
 		scanner := lib.NewScanner(&sourceBuf)
 		tokens := scanner.Scan()
 
-		expectedTokenLen := 15
+		expectedTokenLen := 28
 		if len(tokens) != expectedTokenLen {
 			t.Errorf("Should return %d token for EOF, got %d", expectedTokenLen, len(tokens))
 		}
 
-		expectedTokens := [15]string{
+		expectedTokens := [28]string{
 			"Customer", "(",
-			"name", "=", "Rajesh Kumar", ",",
-			"age", "=", "50", ",",
-			"email", "=", "rajesh@kumar.com",
+			"name", "=", "Michael Scott", ",",
+			"age", "=", "40", ",",
+			"email", "=", "michael@dundermifflin.com", ",",
+			"address", "=",
+			"Address", "(",
+			"billingAddress", "=", "Some random address", ",",
+			"shippingAddress", "=", "Some random address again",
+			")",
 			")"}
 
 		for i := 0; i < len(expectedTokens); i++ {
@@ -177,18 +180,21 @@ func Test_ScanTheCustomerWithMultipleFieldsNested(t *testing.T) {
 			}
 		}
 	})
-	t.Run("Customer(name, age, email): TokenTypes", func(t *testing.T) {
+	t.Run("Customer(name, age, email, Address(billingAddress,shippingAddress)): TokenTypes", func(t *testing.T) {
 		var sourceBuf bytes.Buffer
 		sourceBuf.WriteString(source)
 		scanner := lib.NewScanner(&sourceBuf)
 		tokens := scanner.Scan()
 
-		expectedTokenTypes := [15]types.TokenType{
+		expectedTokenTypes := [28]types.TokenType{
 			types.CLASS_NAME, types.PAREN_OPEN,
 			types.KEY, types.EQUALS, types.VALUE, types.COMMA,
 			types.KEY, types.EQUALS, types.VALUE, types.COMMA,
+			types.KEY, types.EQUALS, types.VALUE, types.COMMA,
+			types.KEY, types.EQUALS, types.CLASS_NAME, types.PAREN_OPEN,
+			types.KEY, types.EQUALS, types.VALUE, types.COMMA,
 			types.KEY, types.EQUALS, types.VALUE,
-			types.PAREN_CLOSE,
+			types.PAREN_CLOSE, types.PAREN_CLOSE,
 			types.EOF}
 
 		for i := 0; i < len(expectedTokenTypes); i++ {
