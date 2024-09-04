eb83456cfe85ef276ad9e05ca5f1d2abea4aad19
Shuvojit <s15sarkar@yahoo.com>
2024-09-04 09:34:20 +0530

    simple conversion works

--- lib/converter.go
+++ lib/converter.go
@@ -1,6 +1,9 @@
 package lib
 
 import (
+	"fmt"
+	"strings"
+
 	"github.com/sarkarshuvojit/lomboktojson/types"
 )
 
@@ -11,5 +14,25 @@ func NewConverter() *Converter {
 }
 
 func (c Converter) Convert(tokens []types.Token) string {
-	return "{}"
+	finals := []string{}
+	for _, token := range tokens {
+		switch token.Type {
+		case types.CLASS_NAME:
+			continue
+		case types.PAREN_OPEN:
+			finals = append(finals, "{")
+		case types.PAREN_CLOSE:
+			finals = append(finals, "}")
+		case types.KEY:
+			finals = append(finals, fmt.Sprintf(`"%s"`, token.Lexeme))
+		case types.EQUALS:
+			finals = append(finals, ":")
+		case types.VALUE:
+			finals = append(finals, fmt.Sprintf(`"%s"`, token.Lexeme))
+		}
+	}
+	if len(finals) == 0 {
+		return "{}"
+	}
+	return strings.Join(finals, " ")
 }
--- lib/converter_test.go
+++ lib/converter_test.go
@@ -1,11 +1,22 @@
 package lib_test
 
 import (
+	"encoding/json"
+	"reflect"
 	"testing"
 
 	"github.com/sarkarshuvojit/lomboktojson/lib"
 	"github.com/sarkarshuvojit/lomboktojson/types"
 )
+
+func deepEquality(a, b string) bool {
+	var ajson map[string]interface{}
+	_ = json.Unmarshal([]byte(a), &ajson)
+	var bjson map[string]interface{}
+	_ = json.Unmarshal([]byte(b), &bjson)
+
+	return reflect.DeepEqual(ajson, bjson)
+}
 
 func Test_ScanSimpleDocument(t *testing.T) {
 	converter := lib.NewConverter()
@@ -21,6 +32,62 @@ func Test_ScanSimpleDocument(t *testing.T) {
 		expected := `{}`
 		got := converter.Convert(inputTokens)
 		if expected != got {
+			t.Errorf("Wanted %s, got %s", expected, got)
+		}
+	})
+}
+
+func Test_ConvertBasicCustomer(t *testing.T) {
+	converter := lib.NewConverter()
+	inputTokens := []types.Token{
+		{
+			Type:    types.CLASS_NAME,
+			Lexeme:  "Customer",
+			Literal: map[string]string{},
+			Line:    0,
+		},
+		{
+			Type:    types.PAREN_OPEN,
+			Lexeme:  "(",
+			Literal: map[string]string{},
+			Line:    0,
+		},
+		{
+			Type:    types.KEY,
+			Lexeme:  "name",
+			Literal: map[string]string{},
+			Line:    0,
+		},
+		{
+			Type:    types.EQUALS,
+			Lexeme:  "=",
+			Literal: map[string]string{},
+			Line:    0,
+		},
+		{
+			Type:    types.VALUE,
+			Lexeme:  "Michael Scott",
+			Literal: map[string]string{},
+			Line:    0,
+		},
+		{
+			Type:    types.PAREN_CLOSE,
+			Lexeme:  ")",
+			Literal: map[string]string{},
+			Line:    0,
+		},
+		{
+			Type:    types.EOF,
+			Lexeme:  "",
+			Literal: map[string]string{},
+			Line:    0,
+		},
+	}
+	t.Run("Should scan simple empty doc", func(t *testing.T) {
+		expected := `{"name": "Michael Scott"}`
+		got := converter.Convert(inputTokens)
+
+		if !deepEquality(expected, got) {
 			t.Errorf("Wanted %s, got %s", expected, got)
 		}
 	})
