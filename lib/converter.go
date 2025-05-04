b75fe52903169facdb8d791fe4fd897d2ff74c9b
Shuvojit <s15sarkar@yahoo.com>
2025-05-05 01:37:29 +0530

    created structure for converter

--- lib/converter.go
+++ lib/converter.go
@@ -1,7 +1,42 @@
 package lib
 
-import "github.com/sarkarshuvojit/lomboktojson/types"
+import (
+	"bytes"
+	"fmt"
+
+	"github.com/sarkarshuvojit/lomboktojson/types"
+)
+
+type TokenConverter func (types.Token) (byte, bool)
+
+var tokenConverterMapping map[types.TokenType]TokenConverter = map[types.TokenType]TokenConverter{
+	types.EOF: func(t types.Token) (byte, bool) {
+		return 0, false
+	},
+	types.CLASS_NAME: func(t types.Token) (byte, bool) {
+		return 0, false
+	},
+}
 
 func Convert(tokens []types.Token) ([]byte, error) {
-	return []byte(`{}`), nil
+	var buf bytes.Buffer
+	buf.WriteByte('{')
+	for _, token := range tokens {
+		if tkn, present := convertToken(token); present {
+			buf.WriteByte(tkn)
+		}
+	}
+
+	buf.WriteByte('}')
+	asBytes := buf.Bytes()
+	fmt.Println(string(asBytes))
+	return asBytes, nil
+}
+
+func convertToken(token types.Token) (byte, bool) {
+	if converterFn, ok := tokenConverterMapping[token.Type]; ok {
+		return converterFn(token)
+	} else {
+		return 0, false
+	}
 }
